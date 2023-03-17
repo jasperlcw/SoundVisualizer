f63b21540d40b30f5bb090e8c26f22ce26df3adc
Jasper Wong <lazypigz.mw3@gmail.com>
2023-03-16 20:57:17 -0700

    Converted LED code to module

--- src/include/ledControl.h
+++ src/include/ledControl.h
@@ -6,16 +6,33 @@
 #ifndef _LED_H_
 #define _LED_H_
 
+#define LED_BLACK   0
+#define LED_RED     1
+#define LED_GREEN   2
+#define LED_YELLOW  3
+#define LED_BLUE    4
+#define LED_MAGENTA 5
+#define LED_TEAL    6
+#define LED_WHITE   7
+
+typedef enum LED_Mode {
+    LED_OFF,
+    LED_CLOCK,
+    LED_LISTEN
+} LED_Mode;
+
 // Initializes the I2C registers for displaying to the LED.
 void LED_init(void);
 
 // Cleans up the I2C registers for displaying to the LED.
 void LED_cleanup(void);
 
-// TODO: figure out a way to give this module data that it can easily convert to display
-// on an LED panel. Maybe a 2D matrix of hex values?
+// Sets the mode of the LED panel for what to display.
+void LED_setMode(LED_Mode mode);
 
-// something along the lines of 
-// void LED_display(2D matrix of hex);
+// Changes the colors displayed by the LED panel, where the passed in matrix
+// should be a size of 16x32 containing values of the defined constants in this module.
+// Note that the program will ignore inputs if the mode is set to CLOCK.
+void LED_setDisplay(int **matrix);
 
 #endif
